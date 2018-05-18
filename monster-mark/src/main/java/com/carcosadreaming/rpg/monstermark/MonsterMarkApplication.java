663286014decb64cd41385dd7b93dad8bcf4a79f
jameskelly <kellyjamesd@gmail.com>
2018-05-17 21:18:35 -0500

    mac changes

--- monster-mark/src/main/java/com/carcosadreaming/rpg/monstermark/MonsterMarkApplication.java
+++ monster-mark/src/main/java/com/carcosadreaming/rpg/monstermark/MonsterMarkApplication.java
@@ -1,15 +1,13 @@
 package com.carcosadreaming.rpg.monstermark;
 
-import com.carcosadreaming.rpg.common.data.model.Classifier;
-import com.carcosadreaming.rpg.common.data.model.jpa.ClassifierJpaImpl;
-import com.carcosadreaming.rpg.common.data.repository.jpa.ClassifierRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -28,15 +26,15 @@ public class MonsterMarkApplication implements CommandLineRunner
   @Override public void run( String... args ) throws Exception
   {
     log.debug("Creating data");
-    ClassifierJpaImpl cls = new ClassifierJpaImpl();
-    cls.setName( "Ability" );
-    ClassifierJpaImpl saved = classifierRepo.save(  cls );
-    log.debug( "Saved:  {}", saved );
-
-    log.debug("returning data");
-    for (Classifier found : classifierRepo.findAll()) {
-      log.error( "Classifier:  {}", found.toString() );
-    }
+//    ClassifierJpaImpl cls = new ClassifierJpaImpl();
+//    cls.setName( "Ability" );
+//    ClassifierJpaImpl saved = classifierRepo.save(  cls );
+//    log.debug( "Saved:  {}", saved );
+//
+//    log.debug("returning data");
+//    for (Classifier found : classifierRepo.findAll()) {
+//      log.error( "Classifier:  {}", found.toString() );
+//    }
   }
 
 }
